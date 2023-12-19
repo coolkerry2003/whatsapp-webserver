e7a17ec2b99957af6e79776ccd68515e989b0ffb
coolkerry2003 <coolkerry2003@gmail.com>
2023-12-19 14:19:15 +0800

    ADJ: add https service

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
 const express = require('express')
+const utf8 = require('utf8');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const test_port = 3000
+const http_port = 8080
+const https_port = 443;
 
-app.use(bodyParser.json());
+var credentials = {
+  //Self Certificate
+  // key: fs.readFileSync('pillaAuth-key.pem', utf8),
+  // cert: fs.readFileSync('pillaAuth-cert.pem', utf8),
+  
+  //Let's Encrypt Certificate
+  key: fs.readFileSync('/etc/letsencrypt/live/vigor3r.servehttp.com/privkey.pem', utf8),
+  cert: fs.readFileSync('/etc/letsencrypt/live/vigor3r.servehttp.com/cert.pem', utf8),
+  ca: fs.readFileSync('/etc/letsencrypt/live/vigor3r.servehttp.com/chain.pem', utf8)
+};
+
 //添加路由
+app.use(bodyParser.json());
 app.use("/api",require("./api"));
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+http.createServer(app).listen(test_port, ()=>{
+  console.log(`Server listening on port ${test_port}`);
+})
+http.createServer(app).listen(http_port, ()=>{
+  console.log(`Server listening on port ${http_port}`);
+})
+https.createServer(credentials, app).listen(https_port, ()=>{
+  console.log(`Server listening on port ${https_port}`);
 });
